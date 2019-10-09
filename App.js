4a526e282fb699579dc7e7a8aeaf22f71a76128e
Alex <alexw.f@hotmail.com>
2019-10-08 22:06:08 -0300

    final

--- App.js
+++ App.js
@@ -23,65 +23,65 @@ function getPrice(data) {
     })[pricesFiltrados.length - 1];
 }
 
-function isDate(d) {
-    if (Object.prototype.toString.call(d) === "[object Date]") {
-        if (isNaN(d.getTime())) {
-            return false;
-        } else {
-            return true;
-        }
-    } else {
-        return false;
-    }
-}
-
 function converterData(strData) {
     var data = new Date(strData);
     var from = strData.split("/");
 
-    data = new Date(from[2], from[1] - 1, from[0]);
-
+    data = new Date(from[2], from[1]-1, from[0]);
     return data;
 }
 
 function dataAtualFormatada(data) {
     dia = data.getDate().toString(),
-        diaF = (dia.length == 1) ? '0' + dia : dia,
-        mes = (data.getMonth() + 1).toString(), //+1 pois no getMonth Janeiro começa com zero.
-        mesF = (mes.length == 1) ? '0' + mes : mes,
-        anoF = data.getFullYear();
+    diaF = (dia.length == 1) ? '0' + dia : dia,
+    mes = (data.getMonth() + 1).toString(),
+    mesF = (mes.length == 1) ? '0' + mes : mes,
+    anoF = data.getFullYear();
+
     return diaF + "/" + mesF + "/" + anoF;
 }
 
+function getSupply(data) {
+    var suppliesFiltrados = supplies.filter(e => {
+        return (converterData(e.date) <= data);
+    });
+
+    return suppliesFiltrados.sort(function (a, b) {
+        if (converterData(a.date) > converterData(b.date)) {
+            return 1;
+        }
+        if (converterData(a.date) < converterData(b.date)) {
+            return -1;
+        }
+        return 0;
+    })[suppliesFiltrados.length - 1];
+}
+
 function getLitros(data) {
-    var supply = supplies.filter(e => {
-        return datasIguais(converterData(e.date), data);
-    })[0];
-
+    var supply = getSupply(data);
     var price = getPrice(data);
-
-    if (!price) {
-        return null;
-    }
-    //console.log(supply.value);
-    //console.log(dataAtualFormatada(data));
 
     return supply.value / price.value;
 }
 
-function datasIguais(a, b) {
-    return a.day == b.day && a.month == b.month && a.year == b.year;
+function getSpent(data) {
+    var spentsFiltrados = spents.filter(e => {
+        return (converterData(e.date) <= data);
+    });
+
+    return spentsFiltrados.sort(function (a, b) {
+        if (converterData(a.date) > converterData(b.date)) {
+            return 1;
+        }
+        if (converterData(a.date) < converterData(b.date)) {
+            return -1;
+        }
+        return 0;
+    })[spentsFiltrados.length - 1];
 }
 
 function getGasto(data) {
-    var spentDoDia = spents.filter(e => {
-        return datasIguais(converterData(e.date), data);
-    })[0];
-
-    if (!spentDoDia) {
-        return null;
-    }
-
+    var spentDoDia = getSpent(data);
     return spentDoDia.value / kmPorL;
 }
 
@@ -110,7 +110,6 @@ app.get('/get-saldos', function (req, res) {
 
     var saldos = [];
     var saldo = 0;
-    var count = 0;
     for (var d = converterData(spentsOrdenado[0].date); d <= converterData(spentsOrdenado[spentsOrdenado.length - 1].date); d.setDate(d.getDate() + 1)) {
         var supplyDoDia = getLitros(d);
         if (supplyDoDia) {
@@ -122,15 +121,7 @@ app.get('/get-saldos', function (req, res) {
             saldo -= gastoDoDia;
         }
 
-        if (count < 20) {
-            console.log(dataAtualFormatada(new Date(d)));
-            console.log(supplyDoDia);
-            console.log(gastoDoDia);
-            console.log(saldo);
-            count++;
-        }
-
-        saldos.push({ date: dataAtualFormatada(new Date(d)), value: saldo.toFixed(2) });
+        saldos.push({ date: dataAtualFormatada(d), value: parseFloat(saldo.toFixed(2)) });
     }
 
     res.json(saldos);
